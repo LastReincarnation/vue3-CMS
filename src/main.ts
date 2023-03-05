948b75b8e333b6e2e9c398b21753ce615b1450ac
dengjie <dengjie@gitlab.com>
2023-03-05 16:27:06 +0800

    feat: integration routet and vuex

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+import store from './store'
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app')
+
+declare module '@vue/runtime-core' {
+    export interface ComponentCustomProperties {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        $store: any
+    }
+}
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state: () => {
+        return {
+            name: 'Jayden'
+        }
+    },
+    mutations: {},
+    actions: {},
+    getters: {}
+})
+
+export default store
